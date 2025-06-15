ad77efb4d9ab1d629541cf0efb48051df980f329
smile1207 <96765192+smile1207@users.noreply.github.com>
2025-06-15 16:30:45 +0800

    Create game_init.d

---
+++ iwantitgood/obj/core/game_init.d
@@ -0,0 +1,6 @@
+obj/core/game_init.o: src/core/game_init.c src/core/game_init.h \
+ src/core/../../architecture.h src/core/../../vector.h \
+ src/core/../systems/card_system.h \
+ src/core/../systems/../../architecture.h \
+ src/core/../systems/character_system.h src/core/../utils/debug_log.h \
+ src/core/../utils/utils.h src/core/../utils/../../architecture.h
